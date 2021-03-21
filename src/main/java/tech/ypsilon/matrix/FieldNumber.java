909a082d1a86bb0a4ab450b382dfee54ca2fc526
Niklas @yDev <niklas@magic-computer.de>
2021-03-21 17:58:47 +0100

    Changes by JK

---
+++ src/main/java/tech/ypsilon/matrix/FieldNumber.java
@@ -0,0 +1,54 @@
+package tech.ypsilon.matrix;
+
+public class FieldNumber implements Comparable<FieldNumber>{
+    
+    private int value;
+    private final int mod; 
+
+    public FieldNumber(int value){
+        this(value, Main.MOD);
+    }
+
+    public FieldNumber(int value, int mod){
+        this.value = value % mod;
+        this.mod = mod;
+    }
+    
+    public FieldNumber add(FieldNumber nbr){
+        if (this.mod == nbr.mod) {
+            return new FieldNumber(this.value + nbr.value, mod);
+        } else {
+            throw new IllegalArgumentException("Modulo not the same");
+        }
+    }
+
+    public FieldNumber multiply(FieldNumber nbr){
+        if(this.mod == nbr.mod){
+            return new FieldNumber(this.value * nbr.value, mod);
+        }else{
+            throw new IllegalArgumentException("Modulo not the same");
+        }
+    }
+
+    public FieldNumber getInverse(){
+        for(int i = 1; i < this.mod; i++){
+            if((this.value + i) % this.mod == 0){
+                return new FieldNumber(i, mod);
+            }
+        }
+        return null;
+    }
+
+    public boolean isZero() {
+        return value % mod == 0;
+    }
+
+    public String toString(){
+        return Integer.toString(value);
+    }
+
+    @Override
+    public int compareTo(FieldNumber o) {
+        return o.value - this.value;
+    }
+}
